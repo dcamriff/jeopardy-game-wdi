011dbc5b752c8e173827946c969239bd6eafb185
dcamriff <driffelatl@gmail.com>
2017-12-12 11:51:14 -0500

    Added .innerHTML to show newScore variable to update the html ID current-score

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ $(document).ready(function() {
     console.log('We are connected to js')
 
     let lastScore = []
+    // set variables for each trivia box level
     const score100 = 100
+    const score200 = 200
+    const score300 = 300
+    const score400 = 400
+    const score500 = 500
+
     $('#gameboard').on('click', '.box', function() {
         let answer = prompt('The animated character who plays a reindeer in the Disney story Frozen. Who is: ')
         if (answer === 'Sven' || 'sven' || 'SVEN') {
             alert("That's correct!")
-            let newScore = lastScore + score100
+            let newScore = (lastScore + score100)
             console.log(newScore)
-            $('.current-score').append('newScore')
+            // Figure out how to return score to the screen
+            //  based on changing variable of newScore
+            document.getElementById('current-score').innerHTML = newScore;
         }
     })
 });
