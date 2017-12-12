27b6d02645d70471adf85b0cd285c5e48ad0de3f
dcamriff <driffelatl@gmail.com>
2017-12-12 14:08:30 -0500

    Added second click box function to test adding scores and showing on html. Removed lastScore variable and just kept one variable called newScore to capture latest value of score.

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
     console.log('We are connected to js')
 
-    let lastScore = []
-    // set variables for each trivia box level
+    // set variable for the sum of all scores up to current leve
+    let newScore = 0
+        // set variables for each trivia box level
     const score100 = 100
     const score200 = 200
     const score300 = 300
     const score400 = 400
     const score500 = 500
+    // set variable for clicking on a level 100 box, level 200 box, etc
+    //  HOLD ON THIS FOR NOW
 
-    $('#gameboard').on('click', '.box', function() {
+    $('#gameboard').on('click', '#box100-c1', function() {
         let answer = prompt('The animated character who plays a reindeer in the Disney story Frozen. Who is: ')
         if (answer === 'Sven' || 'sven' || 'SVEN') {
             alert("That's correct!")
-            let newScore = (lastScore + score100)
+            newScore += 100
             console.log(newScore)
             // Figure out how to return score to the screen
             //  based on changing variable of newScore
             document.getElementById('current-score').innerHTML = newScore;
         }
+
+        $('#gameboard').on('click', '#box200-c1', function() {
+            let answer = prompt('The animated character who plays a reindeer in the Disney story Frozen. Who is: ')
+            if (answer === 'Sven' || 'sven' || 'SVEN') {
+                alert("That's correct!")
+                newScore += 200
+                console.log(newScore)
+                // Figure out how to return score to the screen
+                //  based on changing variable of newScore
+                document.getElementById('current-score').innerHTML = newScore;
+            }
+        })
     })
 });
