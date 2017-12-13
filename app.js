4670a5accb6ab6d8f247fcaa8514a7d562491828
dcamriff <driffelatl@gmail.com>
2017-12-13 12:58:47 -0500

    Working on objects for app.js

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
 $(document).ready(function () {
     console.log('We are connected to js')
 
-    // set variable for the sum of all scores up to current leve
+    // set variable for the sum of all scores up to current level
     let newScore = 0
-    // set variables for each trivia box level
-    // const score100 = 100
-    // const score200 = 200
-    // const score300 = 300
-    // const score400 = 400
-    // const score500 = 500
-    // set variable for clicking on a level 100 box, level 200 box, etc
-    //  HOLD ON THIS FOR NOW
-
-    // TRIVIA QUESTIONS
-    let questions = [
-    {
+    
+    // DATA MANAGEMENT - questions for gameboard boxes/tiles
+    let triviaBoxes = [
+        {
         boxId: 'box1-c1',
         question: 'The name of the reindeer belonging to Kristoff in the Disney story Frozen. Who is ...',
-        answer1: 'Sven',
+        answer: 'Sven',
         points: 100,
         }
+
+        {
+        boxId: 'box2-c1',
+        question: 'tbd',
+        answer: 'tbd',
+        points: 200,
+        }
+
+        {
+        boxId: 'box3-c1',
+        question: 'tbd',
+        answer: 'tbd',
+        points: 200,
+        }
+
     ]
+
+    // ALERTS
+    const correctAlert1 = alert("That's correct! You're on a roll!")
+    const correctAlert2 = alert('Fantastic! You answered correctly!')
+    const correctAlert3 = alert("Way to go! You are correct!")
+    const incorrectAlert1 = alert('Not quite, but good try!')
+    const incorrectAlert2 = alert("Oh, that's not it, but keep trying!")
+    const incorrectAlert3 = alert("Hmm, that's not it, but try another!")
+
+    // GLOBAL FUNCTIONS
+    function getIndex() {
+        $(this).attr('data-box');
+        if 
+
+    }
+
+
 //   THIS ONE HERE IS MASTER THAT IS WORKING -- DO NOT TOUCH BELOW!!
     $('#gameboard').on('click', '#box1-c1', function () {
         let answer = prompt('The animated character who plays a reindeer in the Disney story Frozen. COMPLETE ANSWER: Who is: ')
@@ -40,20 +64,20 @@ $(document).ready(function () {
     });
 //  DO NOT CHANGE ABOVE!!
 
-    // $('.box').on('click', function () {
-    //     let question = $(this.).attr('data-box')
-    //     let answer = prompt(question)
-    //     if ((answer === $(this).attr('answer1') || $(this).attr('answer2') || $(this).attr('answer3')) && (answer !== '')) {
-    //         alert("That's correct!")
-    //         newScore += 200
-    //     }
-    //     else {
-    //         alert("Sorry, that's incorrect")
-    //         newScore -= 200
-    //     }
-    //     $(this.box).replaceWith("<div class='used-box'>played</div>")
-    //     $('#current-score').html(newScore)
-    // });
+    $('.box').on('click', function () {
+        let question = $(this.).attr('data-box')
+        let answer = prompt(question)
+        if ((answer === $(this).attr('answer1')) && (answer !== '')) {
+            correctAlert1()
+            newScore += 200
+        }
+        else {
+            incorrectAlert1()
+            newScore -= 200
+        }
+        $(this.box).replaceWith("<div class='used-box'>played</div>")
+        $('#current-score').html(newScore)
+    });
 
     $('#gameboard').on('click', '#box3-c1', function () {
         let answer = (prompt("The animal's name that leads Santa's sleigh. >> Who is ... ").toUpperCase())
