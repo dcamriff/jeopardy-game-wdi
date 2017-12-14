f2f1f1f91993a261fe245918a21c9fc151382cdd
dcamriff <driffelatl@gmail.com>
2017-12-13 23:44:51 -0500

    Added up to 10 questions and answers and added to incorrect alerts, the correct answers for users. Also added a count on boxesPlayed to alert upon game completion.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
 
     // set variable for the sum of all scores up to current level
     let newScore = 0
-    
+    let boxesPlayed = 0
+    console.log(boxesPlayed)
+
     // DATA MANAGEMENT - questions for gameboard boxes/tiles
     // let triviaBoxes = [
     //     {
@@ -36,10 +38,10 @@ $(document).ready(function () {
     // const incorrectAlert3 = alert("Hmm, that's not it, but try another!")
 
     // PSEUDOCODE IT...
-// When a box is clicked
-// create a variable that looks up the clicked on box's id
-// take that new variable and set it equal to the array index
-// find the object attributes of that index
+    // When a box is clicked
+    // create a variable that looks up the clicked on box's id
+    // take that new variable and set it equal to the array index
+    // find the object attributes of that index
     // $('#gameboard').on('click', function (event) {
     //     // let indexNum = parseInt.data(box)
     //     $('div').click(handler).find('#id')
@@ -56,67 +58,74 @@ $(document).ready(function () {
     //     $(this.box).replaceWith("<div class='used-box'>played</div>")
     //     $('#current-score').html(newScore)
     // });
-// TESTING ABOVE
-// 
-    
-//   THIS ONE HERE IS MASTER THAT IS WORKING -- DO NOT TOUCH BELOW!!
+    // TESTING ABOVE
+    // 
+
+    //   THIS ONE HERE IS MASTER THAT IS WORKING -- DO NOT TOUCH BELOW!!
     $('#gameboard').on('click', '#0', function () {
         let answer = (prompt("He lives in a pineapple under the sea.").toUpperCase())
         if ((answer === 'WHO IS SPONGE BOB SQUARE PANTS?') && (answer !== '')) {
             alert("That's correct!")
             newScore += 100
-            console.log(newScore)
         }
         else {
-            alert('Not quite, but good try!')
+            alert("Not quite, but good try! The correct answer is: 'WHO IS SPONGE BOB SQUARE PANTS?'")
             newScore -= 100
         }
         // make box no longer clickable
         $('#0').replaceWith("<div class='used-box'>played</div>")
         // update score
         $('#current-score').html(newScore)
+        boxesPlayed += 1
+        console.log(boxesPlayed)
     });
 
     $('#gameboard').on('click', '#1', function () {
         let answer = (prompt("He only wears a cape and underwear.").toUpperCase())
-            if ((answer === 'WHO IS CAPTAIN UNDERPANTS?') && (answer !== '')) {
+        if ((answer === 'WHO IS CAPTAIN UNDERPANTS?') && (answer !== '')) {
             alert("Way to go! You got it right!")
             newScore += 200
         }
         else {
-            alert("Oh, that's not it, but keep trying!")
+            alert("Oh, that's not it, but keep playing! The correct answer is: 'WHO IS CAPTAIN UNDERPANTS?'")
             newScore -= 200
         }
         $('#1').replaceWith("<div class='used-box'>played</div>")
         $('#current-score').html(newScore)
+        boxesPlayed += 1
+        console.log(boxesPlayed)
     });
 
     $('#gameboard').on('click', '#2', function () {
         let answer = (prompt("_____ the Explorer wears orange shorts with her purple backpack.").toUpperCase())
         if ((answer === 'WHO IS DORA?') && (answer !== '')) {
-        alert("That's correct! You're on a roll!")
-        newScore += 300
-    }
-    else {
-        alert("Hmm, that's not it, but try another!")
+            alert("That's correct! You're on a roll!")
+            newScore += 300
+        }
+        else {
+            alert("Hmm, that's not it, but try another! The correct answer is: 'WHO IS DORA?'")
             newScore -= 300
         }
         $('#2').replaceWith("<div class='used-box'>played</div>")
         $('#current-score').html(newScore)
+        boxesPlayed += 1
+        console.log(boxesPlayed)
     });
 
     $('#gameboard').on('click', '#3', function () {
         let answer = (prompt("This monkey doesn't wear pants, but the man in the yellow hat does.").toUpperCase())
         if ((answer === 'WHO IS CURIOUS GEORGE?') && (answer !== '')) {
-        alert("Way to go! You are correct!")
-        newScore += 400
-    }
-    else {
-        alert("Oh, that's not it, but keep trying!")
+            alert("Way to go! You are correct!")
+            newScore += 400
+        }
+        else {
+            alert("Oh, that's not it, but keep playing! The correct answer is: 'WHO IS CURIOUS GEORGE?'")
             newScore -= 400
         }
         $('#3').replaceWith("<div class='used-box'>played</div>")
         $('#current-score').html(newScore)
+        boxesPlayed += 1
+        console.log(boxesPlayed)
     });
 
     $('#gameboard').on('click', '#4', function () {
@@ -127,23 +136,25 @@ $(document).ready(function () {
             console.log(newScore)
         }
         else {
-            alert('Not quite, but good try!')
+            alert("Not quite, but good try! The correct answer is: 'WHO IS MICKEY MOUSE?'")
             newScore -= 500
         }
         // make box no longer clickable
         $('#4').replaceWith("<div class='used-box'>played</div>")
         // update score
         $('#current-score').html(newScore)
+        boxesPlayed += 1
+        console.log(boxesPlayed)
     });
 
     $('#gameboard').on('click', '#5', function () {
         let answer = (prompt("Don't even talk to mom until she's had her first cup of ______ in the morning.").toUpperCase())
-            if ((answer === 'WHAT IS COFFEE?') && (answer !== '')) {
+        if ((answer === 'WHAT IS COFFEE?') && (answer !== '')) {
             alert("Way to go! You got it right!")
             newScore += 100
         }
         else {
-            alert("Oh, that's not it, but keep trying!")
+            alert("Oh, that's not it, but keep trying! The correct answer is: 'WHAT IS COFFEE?'")
             newScore -= 100
         }
         $('#5').replaceWith("<div class='used-box'>played</div>")
@@ -151,13 +162,13 @@ $(document).ready(function () {
     });
 
     $('#gameboard').on('click', '#6', function () {
-        let answer = (prompt("Parents are always posting ____ to Instagram or Facebook of the silly things I do.").toUpperCase())
-        if ((answer === 'WHAT ARE PHOTOS?' || answer === 'WHAT ARE PICTURES?' && (answer !== '')) {
-        alert("That's correct! Way to go superstar!")
-        newScore += 200
-    }
-    else {
-        alert("Hmm, that's not it, but try another!")
+        let answer = (prompt("Looking down and ____ on your phone while driving, doesn't set a very good example.").toUpperCase())
+        if ((answer === 'WHAT IS TEXTING?') && (answer !== '')) {
+            alert("That's correct! Way to go superstar! ")
+            newScore += 200
+        }
+        else {
+            alert("Hmm, that's not it, but try another! The correct answer is: 'WHAT IS TEXTING?'")
             newScore -= 200
         }
         $('#6').replaceWith("<div class='used-box'>played</div>")
@@ -165,13 +176,13 @@ $(document).ready(function () {
     });
 
     $('#gameboard').on('click', '#7', function () {
-        let answer = (prompt("T").toUpperCase())
-        if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("Way to go! You are correct!")
-        newScore += 300
-    }
-    else {
-        alert("Oh, that's not it, but keep trying!")
+        let answer = (prompt("Statistics show that learning to play this musical instrument that has black and white keys, can improve your math score.").toUpperCase())
+        if ((answer === 'WHAT IS A PIANO?' || answer === 'WHAT IS THE PIANO?') && (answer !== '')) {
+            alert("Way to go! You are correct!")
+            newScore += 300
+        }
+        else {
+            alert("Oh, that's not it, but keep trying! The correct answer is: 'WHAT IS A PIANO?' or 'WHAT IS THE PIANO?' ")
             newScore -= 300
         }
         $('#7').replaceWith("<div class='used-box'>played</div>")
@@ -179,14 +190,14 @@ $(document).ready(function () {
     });
 
     $('#gameboard').on('click', '#8', function () {
-        let answer = (prompt("T").toUpperCase())
-        if ((answer === 'WHO IS RUDOLPH?') && (answer !== '')) {
+        let answer = (prompt("Parents LOVE to post pictures to ____ or Facebook of the silly things kids do.").toUpperCase())
+        if ((answer === 'WHAT IS INSTAGRAM?') && (answer !== '')) {
             alert("That's correct!")
             newScore += 400
             console.log(newScore)
         }
         else {
-            alert('Not quite, but good try!')
+            alert("Not quite, but good try! The correct answer is: 'WHAT IS INSTAGRAM?'")
             newScore -= 400
         }
         // make box no longer clickable
@@ -196,13 +207,13 @@ $(document).ready(function () {
     });
 
     $('#gameboard').on('click', '#9', function () {
-        let answer = (prompt("T").toUpperCase())
-            if ((answer === 'WHO IS MICKEY?') && (answer !== '')) {
+        let answer = (prompt("This embarrasing line dance, that our parents sometimes still perform at wedding receptions, was made popular in the 80s.").toUpperCase())
+        if ((answer === 'WHAT IS THE ELECTRIC SLIDE?') && (answer !== '')) {
             alert("Way to go! You got it right!")
             newScore += 500
         }
         else {
-            alert("Oh, that's not it, but keep trying!")
+            alert("Oh, that's not it, but keep playing! The correct answer is: 'WHAT IS THE ELECTRIC SLIDE?'")
             newScore -= 500
         }
         $('#9').replaceWith("<div class='used-box'>played</div>")
@@ -212,11 +223,11 @@ $(document).ready(function () {
     $('#gameboard').on('click', '#10', function () {
         let answer = (prompt("This sign").toUpperCase())
         if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("That's correct! You're on a roll!")
-        newScore += 100
-    }
-    else {
-        alert("Hmm, that's not it, but try another!")
+            alert("That's correct! You're on a roll!")
+            newScore += 100
+        }
+        else {
+            alert("Hmm, that's not it, but try another!")
             newScore -= 100
         }
         $('#10').replaceWith("<div class='used-box'>played</div>")
@@ -226,11 +237,11 @@ $(document).ready(function () {
     $('#gameboard').on('click', '#11', function () {
         let answer = (prompt("The name of the reindeer belonging to Kristoff in the Disney story Frozen.").toUpperCase())
         if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("Way to go! You are correct!")
-        newScore += 200
-    }
-    else {
-        alert("Oh, that's not it, but keep trying!")
+            alert("Way to go! You are correct!")
+            newScore += 200
+        }
+        else {
+            alert("Oh, that's not it, but keep trying!")
             newScore -= 200
         }
         $('#11').replaceWith("<div class='used-box'>played</div>")
@@ -256,7 +267,7 @@ $(document).ready(function () {
 
     $('#gameboard').on('click', '#13', function () {
         let answer = (prompt("He's a mouse with big ears and bright yellow shoes.").toUpperCase())
-            if ((answer === 'WHO IS MICKEY?') && (answer !== '')) {
+        if ((answer === 'WHO IS MICKEY?') && (answer !== '')) {
             alert("Way to go! You got it right!")
             newScore += 400
         }
@@ -271,11 +282,11 @@ $(document).ready(function () {
     $('#gameboard').on('click', '#14', function () {
         let answer = (prompt("The name of the reindeer belonging to Kristoff in the Disney story Frozen.").toUpperCase())
         if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("That's correct! You're on a roll!")
-        newScore += 500
-    }
-    else {
-        alert("Hmm, that's not it, but try another!")
+            alert("That's correct! You're on a roll!")
+            newScore += 500
+        }
+        else {
+            alert("Hmm, that's not it, but try another!")
             newScore -= 500
         }
         $('#14').replaceWith("<div class='used-box'>played</div>")
@@ -285,11 +296,11 @@ $(document).ready(function () {
     $('#gameboard').on('click', '#15', function () {
         let answer = (prompt("The name of the reindeer belonging to Kristoff in the Disney story Frozen.").toUpperCase())
         if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("Way to go! You are correct!")
-        newScore += 100
-    }
-    else {
-        alert("Oh, that's not it, but keep trying!")
+            alert("Way to go! You are correct!")
+            newScore += 100
+        }
+        else {
+            alert("Oh, that's not it, but keep trying!")
             newScore -= 100
         }
         $('#15').replaceWith("<div class='used-box'>played</div>")
@@ -315,7 +326,7 @@ $(document).ready(function () {
 
     $('#gameboard').on('click', '#17', function () {
         let answer = (prompt("He's a mouse with big ears and bright yellow shoes.").toUpperCase())
-            if ((answer === 'WHO IS MICKEY?') && (answer !== '')) {
+        if ((answer === 'WHO IS MICKEY?') && (answer !== '')) {
             alert("Way to go! You got it right!")
             newScore += 300
         }
@@ -330,29 +341,35 @@ $(document).ready(function () {
     $('#gameboard').on('click', '#18', function () {
         let answer = (prompt("The name of the reindeer belonging to Kristoff in the Disney story Frozen.").toUpperCase())
         if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("That's correct! You're on a roll!")
-        newScore += 400
-    }
-    else {
-        alert("Hmm, that's not it, but try another!")
+            alert("That's correct! You're on a roll!")
+            newScore += 400
+        }
+        else {
+            alert("Hmm, that's not it, but try another!")
+            newScore -= 400
+        }
+        $('#18').replaceWith("<div class='used-box'>played</div>")
+        $('#current-score').html(newScore)
+    });
+
+    $('#gameboard').on('click', '#19', function () {
+        let answer = (prompt("The name of the reindeer belonging to Kristoff in the Disney story Frozen.").toUpperCase())
+        if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
+            alert("That's correct! You're on a roll!")
+            newScore += 400
+        }
+        else {
+            alert("Hmm, that's not it, but try another!")
             newScore -= 400
         }
         $('#19').replaceWith("<div class='used-box'>played</div>")
         $('#current-score').html(newScore)
     });
 
-    $('#gameboard').on('click', '#18', function () {
-        let answer = (prompt("The name of the reindeer belonging to Kristoff in the Disney story Frozen.").toUpperCase())
-        if ((answer === 'WHO IS SVEN?') && (answer !== '')) {
-        alert("That's correct! You're on a roll!")
-        newScore += 400
-    }
-    else {
-        alert("Hmm, that's not it, but try another!")
-            newScore -= 400
+    function() {
+        if (boxesPlayed >=20) {
+            alert("Congratulations! You've answered all trivia questions!")
         }
-        $('#19').replaceWith("<div class='used-box'>played</div>")
-        $('#current-score').html(newScore)
-    });
+    }
 
 });
